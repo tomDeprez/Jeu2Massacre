2e528818ca0d37e6f43b7dd1e81a6592e651393d
Romain MUNIER <munierr@orange.fr>
2021-03-05 17:32:03 +0100

    Modif JS

---
+++ Projet/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Jeu2Massacre</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <div class="row">
+                    <div class="col-12" id="dropzone">
+                        <div class="row">
+                            <div class="col-12" style="height: 100%; border-bottom: 1px solid;">
+                                <div class="row">
+                                    <div class="col-10">
+                                        <div class="row">
+                                            <div class="col-2" style="height: 100%;">
+                                                <div class="row justify-content-center">
+                                                    <img class="element" src="img/can2.png" height="100px">
+                                                </div>
+                                            </div>
+                                            <div class="col-2" style="height: 100%;">
+                                                <div class="row justify-content-center">
+                                                    <img class="element" src="https://fakeimg.pl/50x100" height="100px">
+
+                                                </div>
+                                            </div>
+                                            <div class="col-2" style="height: 100%;">
+                                                <div class="row justify-content-center">
+                                                    <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                                                </div>
+                                            </div>
+                                            <div class="col-2" style="height: 100%;">
+                                                <div class="row justify-content-center">
+                                                    <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                                                </div>
+                                            </div>
+                                            <div class="col-2" style="height: 100%;">
+                                                <div class="row justify-content-center">
+                                                    <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="row">
+                                                    <div class="col-12 space-30"></div>
+                                                    <button class="col-6" style="height: 50px;">
+                                                        <div class="row justify-content-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="40"
+                                                                height="40" viewBox="0 0 24 24" fill="none"
+                                                                stroke="currentColor" stroke-width="2"
+                                                                stroke-linecap="round" stroke-linejoin="round"
+                                                                class="feather feather-plus-circle">
+                                                                <circle cx="12" cy="12" r="10"></circle>
+                                                                <line x1="12" y1="8" x2="12" y2="16"></line>
+                                                                <line x1="8" y1="12" x2="16" y2="12"></line>
+                                                            </svg>
+                                                        </div>
+                                                    </button>
+                                                    <button class="col-6" style="height: 50px;">
+                                                        <div class="row justify-content-center">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="40"
+                                                                height="40" viewBox="0 0 24 24" fill="none"
+                                                                stroke="currentColor" stroke-width="2"
+                                                                stroke-linecap="round" stroke-linejoin="round"
+                                                                class="feather feather-plus-circle">
+                                                                <circle cx="12" cy="12" r="10"></circle>
+                                                                <line x1="12" y1="8" x2="12" y2="16"></line>
+                                                                <line x1="8" y1="12" x2="16" y2="12"></line>
+                                                            </svg>
+                                                        </div>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="row justify-content-center">
+                                            <button class="col-6">
+                                                <div class="row">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="col-12 feather feather-rotate-ccw">
+                                                        <polyline points="1 4 1 10 7 10"></polyline>
+                                                        <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                            <button class="col-6">
+                                                <div class="row">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="col-12 feather feather-flag">
+                                                        <path
+                                                            d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z">
+                                                        </path>
+                                                        <line x1="4" y1="22" x2="4" y2="15"></line>
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                            <button class="col-6">
+                                                <div class="row">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="col-12 feather feather-trash-2">
+                                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                                        <path
+                                                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                        </path>
+                                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                            <button class="col-6">
+                                                <div class="row">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="col-12 feather feather-refresh-cw">
+                                                        <polyline points="23 4 23 10 17 10"></polyline>
+                                                        <polyline points="1 20 1 14 7 14"></polyline>
+                                                        <path
+                                                            d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15">
+                                                        </path>
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="cardPile" style="position: relative; top:100px">
+                            <div class="row"
+                                style="position: absolute; top: 0; left: 50%; transform: translate(-50%,0);">
+                                <div class="col-12">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                            </div>
+                            <div class="row"
+                                style="position: absolute; top: 110px; left: 50%; transform: translate(-50%,0);">
+                                <div class="col-6">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-6">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                            </div>
+                            <div class="row"
+                                style="position: absolute; top: 220px; left: 50%; transform: translate(-50%,0);">
+                                <div class="col-4">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-4">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-4">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                            </div>
+                            <div class="row"
+                                style="position: absolute; top: 330px; left: 50%; transform: translate(-50%,0);">
+                                <div class="col-3">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                                <div class="col-3">
+                                    <div style="border: 2px solid red; width: 100px; height: 110px;"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-1"></div>
+            <div class="col-2">
+                <div class="row justify-content-center">
+                    <div class="col-12">Drag Winston into the rectangle</div>
+                    <div class="add-picture col-12">
+                        <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                    </div>
+                    <div class="col-12 space-10"></div>
+                    <div class="col-4"></div>
+                    <button class="col-4">
+                        <div class="row">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="col-12 feather feather-rotate-ccw">
+                                <polyline points="1 4 1 10 7 10"></polyline>
+                                <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
+                            </svg>
+                        </div>
+                    </button>
+                    <div class="col-4"></div>
+                    <div class="col-12 space-10"></div>
+                    <div class="add-picture col-6" style="height: 100%;">
+                        <div class="row justify-content-center">
+                            <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                        </div>
+                    </div>
+                    <div class="add-picture col-6" style="height: 100%;">
+                        <div class="row justify-content-center">
+                            <img class="" src="https://fakeimg.pl/50x100" height="100px">
+                        </div>
+                    </div>
+                    <div class="col-12 space-10"></div>
+                    <div class="col-2"></div>
+                    <button class="col-4">
+                        <div class="row">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="col-12 feather feather-rotate-ccw">
+                                <polyline points="1 4 1 10 7 10"></polyline>
+                                <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
+                            </svg>
+                        </div>
+                    </button>
+                    <button class="col-4">
+                        <div class="row">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="col-12 feather feather-rotate-ccw">
+                                <polyline points="1 4 1 10 7 10"></polyline>
+                                <path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path>
+                            </svg>
+                        </div>
+                    </button>
+                    <div class="col-2"></div>
+                    <div class="col-12 space-10"></div>
+                    <img id="ballon" class="" src="https://fakeimg.pl/50x100" height="100px">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="js/script.js"></script>
+
+</html>
